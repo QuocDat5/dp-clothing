f9008c2758f01533c0923ff550c4a4e6cc2a2266
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-09 10:52:24 +0700

    hero headings

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,10 +9,27 @@ const Hero = () => {
         <section className="bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
             <div className="container mx-auto flex justify-around h-full">
                 {/* text */}
-                <div className="flex">
-                    <div>
-                        <div>New trend</div>
+                <div className="flex flex-col justify-center">
+                    {/* pre-title */}
+                    <div className="font-semibold flex items-center uppercase">
+                        <div className="w-10 h-[2px] bg-red-500 mr-3"></div>New
+                        trend
                     </div>
+
+                    {/* title */}
+                    <h1 className="text-[70px] leading-[1.1] font-light mb-4">
+                        AUTUMN SALE
+                        <br />
+                        <span className="font-semibold">WOMEN</span>
+                    </h1>
+
+                    {/* discover-more button */}
+                    <Link
+                        to={"/"}
+                        className="self-start uppercase font-semibold border-b-2 border-primary"
+                    >
+                        Discover more
+                    </Link>
                 </div>
 
                 {/* image */}
